0a3f449b9aba90b80bc0ee71086bd5be46652e61
turboloong <2312987772@qq.com>
2023-03-16 08:27:21 +0800

    注释

--- parse.js
+++ parse.js
@@ -30,34 +30,53 @@ function parseChildren(context, ancestors) {
       const s = context.source;
       let node = null;
       if (startsWith(s, delimiters[0])) {
-          // '{{'
+          // 解析 '{{' '}}'
           node = parseInterpolation(context)
         } else if(s[0] === '<'){
           if (/[a-z]/i.test(s[1])){
+            // 解析 dom
               node  = parseElement(context, ancestors)
           }
       }
       if (!node) {
+        // 解析文字 '/n    '换行符也是
           node = parseText(context)
       }
-  
-      if (Array.isArray(node)) {
-          for (let i = 0; i < node.length; i++) {
-              nodes.push(node[i])
-          }
-      } else {
-          nodes.push(node)
-      } 
+      nodes.push(node)
     }
+
+    // 将循环nodes, 换行符置为空
     for (let i = 0; i < nodes.length; i++) {
       const node = nodes[i]
       if (node.type === NodeTypes.TEXT && !/[^\t\r\n\f ]/.test(node.content)) {
         nodes[i] = null
       }
     }
+    // 过滤掉空node
     return nodes.filter(Boolean)
 }
 
+// 解析dom
+function parseElement(context, ancestors) {
+  // Start tag.
+const element = parseTag(context, TagType.Start)
+
+// Children.
+ancestors.push(element)
+const children = parseChildren(context, ancestors)
+ancestors.pop()
+
+element.children = children
+
+// End tag. 最后一个是空字符串
+if (startsWithEndTagOpen(context.source, element.tag)) {
+  parseTag(context, TagType.End)
+}
+
+return element
+}
+
+// 解析文字或换行符，以到dom起始标签'<'或'{{'为止
 function parseText(context) {
   const endTokens = ['<', delimiters[0]]
   let endIndex = context.source.length
@@ -88,25 +107,8 @@ const TagType = {
     Start: 'Start',
     End: 'End'
 }
-function parseElement(context, ancestors) {
-    // Start tag.
-  const element = parseTag(context, TagType.Start)
 
-  // Children.
-  ancestors.push(element)
-  const children = parseChildren(context, ancestors)
-  ancestors.pop()
-
-  element.children = children
-
-  // End tag. 最后一个是空字符串
-  if (startsWithEndTagOpen(context.source, element.tag)) {
-    parseTag(context, TagType.End)
-  }
-
-  return element
-}
-
+// 解析标签属性，不管是class还是model，还是事件，全都放进props
 function parseTag(
     context,
     type
@@ -134,12 +136,9 @@ function parseTag(
       return
     }
   
-    // let tagType = ElementTypes.ELEMENT
-  
     return {
       type: NodeTypes.ELEMENT,
       tag,
-    //   tagType,
       props,
       isSelfClosing,
       children: [],
@@ -198,103 +197,104 @@ function parseAttribute(
     }
   }
 
-  function parseAttributeValue(context) {
-    let content = '';
-  
-    const quote = context.source[0]
-    const isQuoted = quote === `"` || quote === `'`
-    if (isQuoted) {
-      // Quoted value.
-      advanceBy(context, 1)
-  
-      const endIndex = context.source.indexOf(quote)
-      if (endIndex === -1) {
-        content = parseTextData(
-          context,
-          context.source.length
-        )
-      } else {
-        content = parseTextData(context, endIndex)
-        advanceBy(context, 1)
-      }
+function parseAttributeValue(context) {
+  let content = '';
+
+  const quote = context.source[0]
+  const isQuoted = quote === `"` || quote === `'`
+  if (isQuoted) {
+    // Quoted value.
+    advanceBy(context, 1)
+
+    const endIndex = context.source.indexOf(quote)
+    if (endIndex === -1) {
+      content = parseTextData(
+        context,
+        context.source.length
+      )
     } else {
-      // Unquoted
-      const match = /^[^\t\r\n\f >]+/.exec(context.source)
-      if (!match) {
-        return undefined
+      content = parseTextData(context, endIndex)
+      advanceBy(context, 1)
+    }
+  } else {
+    // Unquoted
+    const match = /^[^\t\r\n\f >]+/.exec(context.source)
+    if (!match) {
+      return undefined
+    }
+    content = parseTextData(context, match[0].length)
+  }
+
+  return { content, isQuoted }
+}
+
+function advanceBy(context, numberOfCharacters) {
+  const { source } = context
+  context.source = source.slice(numberOfCharacters)
+}
+
+function advanceSpaces(context) {
+  const match = /^[\t\r\n\f ]+/.exec(context.source)
+  if (match) {
+    advanceBy(context, match[0].length)
+  }
+}
+
+// 
+function parseInterpolation(
+  context,
+) {
+  const [open, close] = delimiters
+  const closeIndex = context.source.indexOf(close, open.length)
+
+  advanceBy(context, open.length)
+  const rawContentLength = closeIndex - open.length
+  const preTrimContent = parseTextData(context, rawContentLength)
+  const content = preTrimContent.trim()
+  advanceBy(context, close.length)
+
+  return {
+    content: {
+      content,
+    },
+  }
+}
+
+function createParserContext(
+  content,
+){
+  return {
+    originalSource: content,
+    source: content,
+  }
+}
+function startsWith(source, searchString) {
+  return source.startsWith(searchString)
+}
+
+function isEnd(
+  context,
+  ancestors
+) {
+  const s = context.source
+  if (startsWith(s, '</')) {
+    // TODO: probably bad performance
+    for (let i = ancestors.length - 1; i >= 0; --i) {
+      if (startsWithEndTagOpen(s, ancestors[i].tag)) {
+        return true
       }
-      content = parseTextData(context, match[0].length)
-    }
-  
-    return { content, isQuoted }
-  }
-
-  function advanceBy(context, numberOfCharacters) {
-    const { source } = context
-    context.source = source.slice(numberOfCharacters)
-  }
-  
-  function advanceSpaces(context) {
-    const match = /^[\t\r\n\f ]+/.exec(context.source)
-    if (match) {
-      advanceBy(context, match[0].length)
     }
   }
+  return !s
+}
 
-  function parseInterpolation(
-    context,
-  ) {
-    const [open, close] = delimiters
-    const closeIndex = context.source.indexOf(close, open.length)
-  
-    advanceBy(context, open.length)
-    const rawContentLength = closeIndex - open.length
-    const preTrimContent = parseTextData(context, rawContentLength)
-    const content = preTrimContent.trim()
-    advanceBy(context, close.length)
-  
-    return {
-      content: {
-        content,
-      },
-    }
-  }
-
-  function createParserContext(
-    content,
-  ){
-    return {
-      originalSource: content,
-      source: content,
-    }
-  }
-  function startsWith(source, searchString) {
-    return source.startsWith(searchString)
-  }
-
-  function isEnd(
-    context,
-    ancestors
-  ) {
-    const s = context.source
-    if (startsWith(s, '</')) {
-      // TODO: probably bad performance
-      for (let i = ancestors.length - 1; i >= 0; --i) {
-        if (startsWithEndTagOpen(s, ancestors[i].tag)) {
-          return true
-        }
-      }
-    }
-    return !s
-  }
-
-  function startsWithEndTagOpen(source, tag) {
-    return (
-      startsWith(source, '</') &&
-      source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase() &&
-      /[\t\r\n\f />]/.test(source[2 + tag.length] || '>')
-    )
-  }
+function startsWithEndTagOpen(source, tag) {
+  return (
+    startsWith(source, '</') &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase() &&
+    /[\t\r\n\f />]/.test(source[2 + tag.length] || '>')
+  )
+}
 export default function parse(content) {
     const context = createParserContext(content)
     const tree = parseChildren(context, [])
